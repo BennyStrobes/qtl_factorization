9a2a3347b244a099ab25ec58189a49277ba03c1a
BennyStrobes <bstrobe1@jhu.edu>
2021-12-10 11:24:24 -0500

    Testing

--- ye_lab_single_cell/driver_key.sh
+++ ye_lab_single_cell/driver_key.sh
@@ -12,7 +12,7 @@
 ######################
 # Input single cell expression data (emailed by Meena Subramaniam on Nov. 18, 2019)
 input_h5py_file="/work-zfs/abattle4/lab_data/ye_lab_lupus_data_set/expression/Lupus_study_adjusted.h5ad"
-
+##
 # Gene annotation file (hg19)
 gene_annotation_file="/work-zfs/abattle4/lab_data/annotation/gencode.v19/gencode.v19.annotation.gene.txt"
 
